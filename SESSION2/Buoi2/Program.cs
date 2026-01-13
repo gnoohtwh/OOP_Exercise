158851dcfaf401c85a016dc962d1738a516b7bd7
gnoohtwh <thongismyname1507@gmail.com>
2026-01-13 15:48:10 +0700

    some initial idea

--- SESSION2/Buoi2/Program.cs
+++ SESSION2/Buoi2/Program.cs
@@ -44,10 +44,10 @@ class Program
 /* ------------LAB 02----------------------
 Một lớp Point gồm có 2 thuộc tính: x, y (float), là toạ độ của điểm trong hệ toạ độ Decartes.
 1/ Hãy khai báo lớp Point trên
-2/ Bổ sung 3 dạng constructor: không tham số, có tham số và copy constructor.
+2/ Bổ sung 3 dạng constructor: không tham số, có tham số và copy constructor. ->ý nói tại 3 method với no parameters, having parameters and copy method
 3/ Bổ sung phương thức ShowÌnfo để in thông tin của một Point dưới dạng: (x,y)
-4/ Bổ sung các phương thức sau: tính khoảng cách giữa 2 Point, kiểm tra một Point thuộc phần tư nào, kiểm tra 2 Point có dói xứng qua trục hoành không
-5/ Trong hàm Mian, tạo ra một List các Point và in ra một bộ 3 Point tạo thành 1 tam giác ( chỉ cần in một bộ ba duy nhất). Phương tháuc này là static method trong class Program ( không phải thành viên của lớp Point). Và kiểm tra các methods trong câu số 4.
+4/ Bổ sung các phương thức sau: tính khoảng cách giữa 2 Point, kiểm tra một Point thuộc phần tư nào, kiểm tra 2 Point có dói xứng qua trục hoành không -> đối xứng tức vuông góc với trục hoành và nối hai điểm đó sẽ tạo đoạn thẳng cắt trục hoành tại trung điẻm
+5/ Trong hàm Main, tạo ra một List các Point và in ra một bộ 3 Point tạo thành 1 tam giác ( chỉ cần in một bộ ba duy nhất). Phương tháuc này là static method trong class Program ( không phải thành viên của lớp Point). Và kiểm tra các methods trong câu số 4.
 
 
 
