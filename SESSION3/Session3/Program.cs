be64ff19467c1daa1bfdc1361aae2f3600658c4f
gnoohtwh <thongismyname1507@gmail.com>
2026-01-20 14:33:25 +0700

    Lab3

---
+++ SESSION3/Session3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Một lớp vector 2D có các thông tin: toạ độ x, toạ độ y (double)
+Hãy bổ sung với Vector class các phương thức sau đây:
+- 3 dạng constructor (no params, with params and copy constructor)
+- Sử dụng method dạng có parameters để cho phép cộng nhiều vector lại với nhau
+- Sử dụng overloading method để cho phép nhân 2 vector hoặc nhân vector với 1 số
+- Bổ sung thêm các method sau: Tính module của vector, kiểm tra 2 vector cùng phương/hướng/vector,tịnh tiến vector theo số bước nhảy được chỉ định
+- Trong hàm main tạo collection các vector và kiểm tra các phương thức được yêu cầu ở trên
+
+
+*/
+public class Course
+{
+    
+}
+class Student
+
+{
+    public int id {get;set;}
+    public string stuName {get;set;}
+    public double GPA {get;set;}
+    
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
